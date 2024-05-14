ee16f039833556b5b762a8f3275ce5fdc0c2b210
wale2009 <adewaleadebayo202@gmail.com>
2024-05-14 01:27:45 +0100

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('./models/product.model');
@@ -5,25 +8,25 @@ const productRoute = require('./routes/product.route');
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
 
-// routes
+// Routes
 app.use('/api/products', productRoute);
-
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API server")
 });
 
-
-mongoose.connect("mongodb+srv://adewaleadebayo202:INiWuVZtp5HHS0l0@backenddb.vnvq3gx.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+// Connect to MongoDB using environment variable
+const dbUrl = process.env.DB_URL;
+mongoose.connect(dbUrl)
     .then(() => {
         console.log("Connected to database!");
         app.listen(4001, () => {
-            console.log("server is running at port 4001");
+            console.log("Server is running at port 4001");
         });
     })
-    .catch(() => {
-        console.log("Could not connect to the")
+    .catch((error) => {
+        console.error("Could not connect to the database:", error);
     });
